553e930faca7e84cc9e9049e9da4e3e2362259a2
kavehkm <mehrbaniankaveh@gmail.com>
2021-12-20 20:21:56 +0330

    create src package as project source code holder and create mgs.py as project entry

---
+++ src/confs.py
@@ -0,0 +1,10 @@
+# standard
+import os
+
+
+# project root
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# google credentials file
+GOOGLE_CREDENTIALS_FILE = os.path.join(BASE_DIR, 'credentials.json')
