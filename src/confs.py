1101ac8e50974dc830c3b048d4b5067aa5fc1708
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-06 12:11:02 +0330

    update confs.py: 1) add new SETTINGS_FILE 2) update DEFAULT_SETTINGS: to hold invoice and customer app settings

--- src/confs.py
+++ src/confs.py
@@ -6,5 +6,22 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+# settings file
+SETTINGS_FILE = os.path.join(BASE_DIR, 'settings.json')
+
+
 # google credentials file
 GOOGLE_CREDENTIALS_FILE = os.path.join(BASE_DIR, 'credentials.json')
+
+
+# default settings
+DEFAULT_SETTINGS = {
+    'database_server': '',
+    'database_username': '',
+    'database_password': '',
+    'database_name': '',
+    'invoice_sheet': 'invoices',
+    'invoice_interval': 10,
+    'customer_sheet': 'customers',
+    'customer_interval': 60
+}
