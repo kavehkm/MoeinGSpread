dd40817161a0a6c95b914a551c027584ec4296e2
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-07 10:19:11 +0330

    update MainWindow: 1) simplify ui

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,7 +1,7 @@
 # internal
-from src.ui.widgets import EngineWidget, SettingsWidget
+from src.ui.widgets import EngineWidget
 # pyqt
-from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout
 
 
 class UI(QMainWindow):
@@ -15,8 +15,6 @@ class UI(QMainWindow):
         self._createUI()
         self._createTray()
         self._connectSignals()
-        # show first widget
-        self._menuHandler(0)
 
     def _createUI(self):
         # central widget
@@ -25,50 +23,18 @@ class UI(QMainWindow):
         # general layout
         self.generalLayout = QHBoxLayout()
         self.centWidget.setLayout(self.generalLayout)
-
         # title
-
-        # geometry
-
-        # min-size
-
-        # icon
-
-        # menu
-        self.menu = QWidget(self)
-        self.menu.setFixedWidth(100)
-        self.menuLayout = QVBoxLayout()
-        self.menu.setLayout(self.menuLayout)
-        self.generalLayout.addWidget(self.menu)
-        # - menu buttons
-        self.btnEngine = QPushButton('Engine')
-        self.btnSettings = QPushButton('Settings')
-        self._menuBtns = [
-            self.btnEngine,
-            self.btnSettings,
-        ]
-        for btn in self._menuBtns:
-            self.menuLayout.addWidget(btn)
-        self.menuLayout.addStretch(1)
-        # widgets
-        self.widget = QWidget(self)
-        self.widgetLayout = QVBoxLayout()
-        self.widget.setLayout(self.widgetLayout)
-        self.generalLayout.addWidget(self.widget)
-        self._widgets = [
-            widgets.EngineWidget(self),
-            widgets.SettingsWidget(self),
-        ]
-        for widget in self._widgets:
-            self.widgetLayout.addWidget(widget)
+        self.setWindowTitle('Updater')
+        # fix size
+        self.setFixedSize(170, 75)
+        # engine widget
+        self.engineWidget = EngineWidget(self)
+        self.generalLayout.addWidget(self.engineWidget)
 
     def _createTray(self):
         pass
 
     def _connectSignals(self):
-        pass
-
-    def _menuHandler(self, index):
         pass
 
     def showNotification(self, title, message):
