5bb5e1553c13b5c99bf62b9c37a84ed7c2f9ab26
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-07 22:05:35 +0330

    add required triggers(insert, update and delete) for Customer table to track activities

--- docs/triggers.sql
+++ docs/triggers.sql
@@ -26,3 +26,42 @@ BEGIN
     FROM deleted
     WHERE type = 2 AND FishNo IS NOT NULL
 END
+---------------------
+-- CUSTOMER-INSERT --
+---------------------
+CREATE TRIGGER MGSCustomerInsert
+ON AshkhasList
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO MGS(id, subject, act)
+    SELECT id, 2, 1
+    FROM inserted
+END
+---------------------
+-- CUSTOMER-UPDATE --
+---------------------
+CREATE TRIGGER MGSCustomerUpdate
+ON AshkhasList
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO MGS(id, subject, act)
+    SELECT id, 2, 2
+    FROM inserted
+END
+---------------------
+-- CUSTOMER-DELETE --
+---------------------
+CREATE TRIGGER MGSCustomerDelete
+ON AshkhasList
+AFTER DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO MGS(id, subject, act)
+    SELECT id, 2, 3
+    FROM deleted
+END
