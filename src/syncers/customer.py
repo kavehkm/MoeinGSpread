f4f72d352d8ff78f35bec707d03599d5b80bda3d
kavehkm <mehrbaniankaveh@gmail.com>
2022-09-02 07:32:38 +0430

    update Customer syncer: 1) implement serialize method

--- src/syncers/customer.py
+++ src/syncers/customer.py
@@ -1,6 +1,32 @@
 # internal
+from src import settings
 from .base import BaseSyncer
+from src.utils import split_tels
+from src.models import Customer, MGS
 
 
 class Customer(BaseSyncer):
     """Customer Syncer"""
+
+    MODEL = Customer
+    TARGETS = settings.g('customer_sheet')
+    SUBJECT = MGS.CUSTOMER
+
+    def serialize(self, instance):
+        record = [
+            instance.id,
+            instance.code,
+            instance.name,
+            instance.email,
+            instance.state,
+            instance.city,
+            instance.address,
+            instance.post_code,
+            instance.company,
+            instance.company_address,
+            instance.info,
+            instance.group_name
+        ]
+        for tel in split_tels(instance.tels):
+            record.append(tel)
+        return record
