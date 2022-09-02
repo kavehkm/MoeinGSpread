0c576915791cda7283ca85050f6393678591a7ca
kavehkm <mehrbaniankaveh@gmail.com>
2022-09-02 07:33:50 +0430

    create new syncer: Invoice

---
+++ src/syncers/invoice.py
@@ -0,0 +1,31 @@
+# internal
+from src import settings
+from .base import BaseSyncer
+from src.utils import split_tels
+from src.models import Invoice, MGS
+
+
+class Invoice(BaseSyncer):
+    """Invoice Syncer"""
+
+    MODEL = Invoice
+    TARGETS = settings.g('invoice_sheet')
+    SUBJECT = MGS.INVOICE
+
+    def serialize(self, instance):
+        record = [
+            instance.id,
+            instance.fish_no,
+            instance.date,
+            instance.time,
+            instance.address,
+            instance.code,
+            instance.name,
+            instance.send_time,
+            instance.info,
+            instance.total,
+            '\n'.join(['{name} * {quantity}'.format(**item) for item in instance.items])
+        ]
+        for tel in split_tels(instance.tel):
+            record.append(tel)
+        return record
