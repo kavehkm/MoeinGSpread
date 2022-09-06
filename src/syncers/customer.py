9838343208c44ddcf3b1b5eb87f99b3dc791a147
kavehkm <mehrbaniankaveh@gmail.com>
2022-09-06 07:38:33 +0430

    update Customer syncer class: 1) update tracker and targets source 2) implement _download method to grab changes and update related model on database

--- src/syncers/customer.py
+++ src/syncers/customer.py
@@ -1,16 +1,17 @@
 # internal
 from src import settings
 from .base import BaseSyncer
-from src.utils import split_tels
 from src.models import Customer, MGS
+from src.utils import split_tels, null_to_none
 
 
 class Customer(BaseSyncer):
     """Customer Syncer"""
 
     MODEL = Customer
-    TARGETS = settings.g('customer_sheet')
     SUBJECT = MGS.CUSTOMER
+    TRACKER = settings.g('customer_tracker')
+    TARGETS = settings.g('customer_targets', [])
 
     def serialize(self, instance):
         record = [
@@ -30,3 +31,34 @@ class Customer(BaseSyncer):
         for tel in split_tels(instance.tels):
             record.append(tel)
         return record
+
+    def _download(self):
+        # set main target
+        target = self.targets[0]
+        # download all update logs
+        logs = self.tracker.get_all_records()
+        for log in logs:
+            # get updated record, clean and collect data
+            record = list(map(null_to_none, target.get_record(log[0])))
+            data = {
+                'id': record[0],
+                'code': record[1],
+                'name': record[2],
+                'email': record[3],
+                'state': record[4],
+                'city': record[5],
+                'address': record[6],
+                'post_code': record[7],
+                'company': record[8],
+                'company_address': record[9],
+                'info': record[10],
+                'group_name': record[11],
+                'tels': [tel for tel in record[12:] if tel]
+            }
+            # find customer and update
+            customer = self.MODEL.get(data['id'])
+            customer.update(data)
+            # delete tracker log
+            self.tracker.delete_record(1)
+            # delete mgs logs
+            MGS.delete_customers(data['id'])
